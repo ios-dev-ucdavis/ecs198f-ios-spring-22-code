c2b266c552a46b5b77f624596c2e91db93268b44
Yibo Yan <totoroq@outlook.com>
2022-03-29 22:44:07 -0700

    Finalized lecture 1.1 codes

---
+++ src/ToDoSwiftUI/ToDoSwiftUI/ToDoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoSwiftUIApp.swift
+//  ToDoSwiftUI
+//
+//  Created by Yibo Yan on 3/29/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
